841ee1a40d16906be053bb75ce85488156261ae9
Shanraj333 <119280949+Shanraj333@users.noreply.github.com>
2025-06-26 22:16:15 +0530

    Update Fizzbuzz_game.py

--- Fizzbuzz_game.py
+++ Fizzbuzz_game.py
@@ -1,9 +1,9 @@
 i = int(input("Enter a number between 1 and 50:"))
-if i % 3 == 0 and i %5 == 0:
+if i % 3 == 0 and i % 5 == 0:
        print("FizzBuzz")
 elif i % 3 == 0:
-        print("Fizz")
+       print("Fizz")
 elif i % 5 == 0:
-        print("Buzz")
+       print("Buzz")
 else:
-        print(i)
+       print(i)
