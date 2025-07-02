1a07b098b329ed643f5df0a109c815f482672ff7
Shanraj333 <119280949+Shanraj333@users.noreply.github.com>
2025-07-02 22:44:47 +0530

    Delete area_of_triangle.py

--- area_of_triangle.py
+++
@@ -1,5 +0,0 @@
-base=int(input("Enter the base in metres:"))
-height=int(input("Enter the height in metres:"))
-area=(1/2)*base*height
-ar=float(area)
-print("Area of the given triangle is:"+ str(ar))
