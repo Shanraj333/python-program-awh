d1ebd21d34eabdcacf26d9f91f04b64c95df711e
Shanraj333 <119280949+Shanraj333@users.noreply.github.com>
2025-07-02 22:43:04 +0530

    Delete Reverse_a_sentence.py

--- Reverse_a_sentence.py
+++
@@ -1,6 +0,0 @@
-def reverse(sentence):
-    rev=" ".join(word[::-1] for word in sentence.split())
-    return rev
-input_string=input("Enter a string:")
-output_string=reverse(input_string)
-print(output_string)
