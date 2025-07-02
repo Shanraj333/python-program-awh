b234f9c8f3e0ef8f96e02f15d24d4bc7b2e514c4
Shanraj333 <119280949+Shanraj333@users.noreply.github.com>
2025-07-02 20:26:04 +0530

    Delete Fizzbuzz_game.py

--- Fizzbuzz_game.py
+++
@@ -1,9 +0,0 @@
-i = int(input("Enter a number between 1 and 50:"))
-if i % 3 == 0 and i % 5 == 0:
-       print("FizzBuzz")
-elif i % 3 == 0:
-       print("Fizz")
-elif i % 5 == 0:
-       print("Buzz")
-else:
-       print(i)
